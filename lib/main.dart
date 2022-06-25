db2e5b0077f5829c7d535f8814ac580a46424cc3
Gektor Adonis <gektoradonis@gmail.com>
2022-06-25 03:20:33 +0300

    Beta version

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,13 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      title: 'Flutter Demo',
+    return const CupertinoApp(
+      title: 'Expense Calculator',
       debugShowCheckedModeBanner: false,
       theme: CupertinoThemeData(
         primaryColor: Colors.amber,
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
@@ -30,34 +30,114 @@ class _MyHomePageState extends State<MyHomePage> {
   TextEditingController? _kilometersController;
   TextEditingController? _fuelPriceController;
   TextEditingController? _exchangeRateController;
+  final carPrice = 15000.0;
+  final consumption = 10.0;
+  final driverPercent = 0.15;
+  double kilometers = 0.0;
+  final generalKilometers = 1000000.0;
+  double amorthization = 0.0;
+  double fuelPrice = 0.0;
+  double euroRate = 0.0;
+  double generalConsumption = 0.0;
+  double generalFuelPrice = 0.0;
+  double generalFuelPriceInEuro = 0.0;
+  final pricePerKilometer = 0.1;
+  double generalPriceForKilometers = 0.0;
+  double driverSalary = 0.0;
+  double total = 0.0;
+  bool isTotalShown = false;
+
   @override
   Widget build(BuildContext context) {
+    void calculateAmorthization() {
+      amorthization = (carPrice / generalKilometers) * kilometers;
+    }
+
+    void calculateGeneralFuelPrice() {
+      generalConsumption = (kilometers / consumption);
+      generalFuelPrice = generalConsumption * fuelPrice;
+    }
+
+    void calculateGeneralFuelPriceInEuro() {
+      calculateGeneralFuelPrice();
+      generalFuelPriceInEuro = generalFuelPrice / euroRate;
+    }
+
+    void calculateExpense() {
+      calculateGeneralFuelPriceInEuro();
+      calculateAmorthization();
+      total = generalFuelPriceInEuro + amorthization;
+    }
+
+    void showTotal() {
+      setState(() {
+        isTotalShown = true;
+      });
+    }
+
     return CupertinoPageScaffold(
+      backgroundColor: Colors.black,
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Container(
-            child: Column(
-              children: [
-                Text("Введіть кілометраж"),
-                CupertinoTextField(
-                  controller: _kilometersController,
-                  keyboardType: TextInputType.number,
-                ),
-                Text('Введіть ціну палива'),
-                CupertinoTextField(
-                  controller: _fuelPriceController,
-                  keyboardType: TextInputType.number,
-                ),
-                Text("Введіть курс євро"),
-                CupertinoTextField(
-                  controller: _exchangeRateController,
-                  keyboardType: TextInputType.number,
-                ),
-                CupertinoButton(
-                    child: Text("Порахувати витрати"), onPressed: () {}),
-              ],
+          const Text(
+            "Введіть кілометраж",
+            style: TextStyle(
+              color: Colors.white,
             ),
+          ),
+          CupertinoTextField(
+            controller: _kilometersController,
+            keyboardType: TextInputType.number,
+            onChanged: (value) {
+              kilometers = value.isEmpty ? 0.0 : double.parse(value);
+            },
+          ),
+          const Text(
+            'Введіть ціну палива',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          CupertinoTextField(
+            controller: _fuelPriceController,
+            keyboardType: TextInputType.number,
+            onChanged: (value) {
+              fuelPrice = value.isEmpty ? 0.0 : double.parse(value);
+            },
+          ),
+          const Text(
+            "Введіть курс євро",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          CupertinoTextField(
+            controller: _exchangeRateController,
+            keyboardType: TextInputType.number,
+            onChanged: (value) {
+              euroRate = value.isEmpty ? 0.0 : double.parse(value);
+            },
+          ),
+          CupertinoButton(
+            color: Colors.greenAccent,
+            child: const Text(
+              "Порахувати витрати",
+            ),
+            onPressed: () {
+              calculateExpense();
+              showTotal();
+            },
+          ),
+          Container(
+            child: isTotalShown
+                ? Text(
+                    "Витрати: " + total.toStringAsFixed(2),
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                  )
+                : null,
           ),
         ],
       ),
